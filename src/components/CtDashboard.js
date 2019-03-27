e3bf91ee42698fb36c1c84305cf92cf7177f545c
Suraj <suraj.mothilal@karmacasting.com>
2019-03-26 20:47:38 -0400

    Adds absolute paths

---
+++ src/components/CtDashboard.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+import { Text } from "react-native";
+
+class CtDashboard extends PureComponent {
+  render() {
+    return <Text>Hey</Text>;
+  }
+}
+
+export default CtDashboard;
