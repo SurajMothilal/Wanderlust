f09a7986678f1ca68683a66d26496107489bae93
Suraj <suraj.mothilal@karmacasting.com>
2019-03-26 20:57:25 -0400

    Adds GoogleMaps support for ios and react native maps

--- src/components/CtDashboard.js
+++ src/components/CtDashboard.js
@@ -1,9 +1,20 @@
 import React, { PureComponent } from "react";
-import { Text } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 class CtDashboard extends PureComponent {
   render() {
-    return <Text>Hey</Text>;
+    return (
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        provider={PROVIDER_GOOGLE}
+        style={{ flex: 1 }}
+      />
+    );
   }
 }
 
